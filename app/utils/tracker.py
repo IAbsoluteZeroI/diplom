6968649f441e862eccbaabcc58f14ac308804e9a
Alexey Golyshev <kasperekd7@yandex.ru>
2024-03-08 17:27:24 +0300

    тест

--- app/utils/tracker.py
+++ app/utils/tracker.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 import cv2
 import numpy as np
 from supervision.tools.detections import Detections
-
+#[eq]
 
 class VideoProcessor:
     def __init__(
--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+mkdir postgres
+mkdir postgres/data
+sudo chown -R $USER:$USER postgres/data
 pip install -r requirements.txt
 cd app/utils/ByteTrack/
 python setup.py develop
