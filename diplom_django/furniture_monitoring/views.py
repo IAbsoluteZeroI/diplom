9a4e8916543975186cdc5491b74203eceeff6a96
KinitoSs <kinburrr@gmail.com>
2024-04-28 21:14:38 +0300

    update views

--- diplom_django/furniture_monitoring/views.py
+++ diplom_django/furniture_monitoring/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import subprocess
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the index.")
+
