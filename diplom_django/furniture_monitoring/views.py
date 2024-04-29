863bfd14ab92e9db0dd00e359ffa01fe815e23a6
KinitoSs <kinburrr@gmail.com>
2024-04-29 14:07:12 +0300

    templates, static

--- diplom_django/furniture_monitoring/views.py
+++ diplom_django/furniture_monitoring/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import subprocess
-
+from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    context = {}
+    return render(request, "furniture_monitoring/index.html", context)
 
