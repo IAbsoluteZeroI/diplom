c8a0a3ec5dbd2ef98450ab9a1d11ce16d83d28e9
KinitoSs <kinburrr@gmail.com>
2024-05-13 16:51:54 +0300

    add postgresql, connect with django

--- diplom_django/diplom_django/settings.py
+++ diplom_django/diplom_django/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = "diplom_django.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db",
+        "USER": "admin",
+        "PASSWORD": "admin",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
