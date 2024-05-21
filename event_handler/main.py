c3cd5b9978863c2657950f3326a003e2a4ff853f
Alexey Golyshev <kasperekd7@yandex.ru>
2024-05-21 17:16:25 +0300

    upd handler

--- event_handler/main.py
+++ event_handler/main.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import aioredis
 from pydantic import BaseModel
 
@@ -10,37 +9,43 @@ class EventInfo(BaseModel):
     event_type: str
 
 
-async def get_all_keys():
-    redis = aioredis.from_url("redis://redis")
-    keys = []
-    cursor = "0"
-    while cursor:
-        cursor, result = await redis.scan(cursor)
-        keys.extend(result)
-    await redis.close()
-    print(keys)
-    return keys
+class Handler:
+    def __init__(self):
+        self.redis = aioredis.from_url("redis://redis")
 
+    async def get_all_keys(self):
+        keys = []
+        cursor = b"0"
+        while cursor:
+            cursor, result = await self.redis.scan(cursor)
+            keys.extend(result)
+        return keys
 
-async def get_all_keys_and_values():
-    redis = aioredis.from_url("redis://redis")
-    keys = []
-    cursor = "0"
-    while cursor:
-        cursor, result = await redis.scan(cursor)
-        keys.extend(result)
+    async def get_all_keys_and_values(self, keys, model: BaseModel):
+        instances = []
+        for key in keys:
+            json_data = await self.redis.get(key)
+            if json_data:
+                instance = model.parse_raw(json_data)
+                instances.append(instance)
+                await self.redis.delete(key)
+        return instances
 
-    values = await redis.mget(*keys)  # получаем значения по всем ключам сразу
-    await redis.close()
-
-    # соединяем ключи и значения в словарь
-    keys_and_values = dict(zip(keys, values))
-    print(keys_and_values)
-    return keys_and_values
+    async def close(self):
+        await self.redis.close()
 
 
 async def main():
-    await get_all_keys_and_values()
+    handler = Handler()
+    try:
+        while True:
+            keys = await handler.get_all_keys()
+            if keys:
+                instances = await handler.get_all_keys_and_values(keys, EventInfo)
+                print(instances)
+            await asyncio.sleep(10)  # Ждем 10 секунд перед следующим циклом
+    finally:
+        await handler.close()
 
 
 if __name__ == "__main__":
