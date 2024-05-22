1059d0c22878f8eda550e46f1b4bebf61c925c51
Alexey Golyshev <kasperekd7@yandex.ru>
2024-05-23 00:01:15 +0300

    Множественный запуск воркеров

--- diplom_django/furniture_monitoring/views.py
+++ diplom_django/furniture_monitoring/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect, render, reverse
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_POST
 
+import threading
 import docker
 client = docker.from_env()
 
@@ -23,6 +24,25 @@ def get_next_worker_name():
             return candidate_name
         i += 1
 
+def start_worker(filepath,cam_id,x1,y1,x2,y2):
+    client.containers.run("diplom-worker",
+                ["python3", "main.py", "--file-path", filepath, "--camera-id", str(cam_id), "--start-xy", str(x1), str(y1), "--end-xy", str(x2), str(y2)],
+                remove=True,
+                name = get_next_worker_name(),
+                network="diplom_default",
+                device_requests=[
+                    docker.types.DeviceRequest(
+                        count=-1,
+                        capabilities=[['gpu']
+                                    ])])
+    
+def stop_worker(worker_name):
+    try:
+        container = client.containers.get(worker_name)
+        container.stop()
+        container.remove()
+    except docker.errors.NotFound:
+        print(f"Container {worker_name} not found.")
 
 # Create your views here.
 def index(request):
@@ -56,6 +76,8 @@ def table_view(request, table_name):
     return render(request, "furniture_monitoring/table.html", context)
 
 
+import threading
+
 @csrf_protect
 def track_cameras_view(request):
     if request.method == "POST":
@@ -67,19 +89,11 @@ def track_cameras_view(request):
                 id__in=selected_counters
             )
             print(selected_line_counters)
-            # Call your function to start tracking with the selected line counters
-            container = client.containers.run("diplom-worker",
-                                              ["python3", "main.py", "--file-path", "kab24.avi", "--camera-id", "1", "--start-xy", "1200", "750", "--end-xy", "1100", "230"],
-                                              remove=True,
-                                              name = get_next_worker_name(),
-                                              network="diplom_default",
-                                              device_requests=[
-                                                  docker.types.DeviceRequest(
-                                                      count=-1,
-                                                      capabilities=[['gpu']
-                                                                    ])])
-
-            print(container.logs().decode('utf-8'))
+            
+            # Создание и запуск потока с правильной передачей аргументов
+            thread = threading.Thread(target=start_worker, args=("kab24.avi", 1, 1200, 750, 1100, 230))
+            thread.start()
+            
             # redirect to the same page after processing
             return redirect(reverse("track_cameras"))
         elif action == "stop_tracking":
@@ -87,13 +101,15 @@ def track_cameras_view(request):
             selected_line_counters = LineCounter.objects.filter(
                 id__in=selected_counters
             )
-            # Call your function to stop tracking here
-            # redirect to the same page after processing
+            # Пример остановки контейнера (имя контейнера должно быть известно)
+            
+            stop_worker("worker_1")
+            
             return redirect(reverse("track_cameras"))
         else:
-            # Handle invalid action if needed
             pass
     else:
         context = {}
         context["line_counters"] = LineCounter.objects.all().order_by("id")[:2]
         return render(request, "furniture_monitoring/track_cameras.html", context)
+
