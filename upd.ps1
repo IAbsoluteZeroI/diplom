4231e39f237d91b825e76e6450848012162ae4ba
KinitoSs <kinburrr@gmail.com>
2024-05-23 14:48:54 +0300

    добавил скрипт обновления в репо

---
+++ upd.ps1
@@ -0,0 +1,14 @@
+# Убедитесь, что Docker установлен и запущен
+docker --version
+
+# Выполните docker-compose down
+docker-compose down
+
+# Выполните git pull
+git pull
+
+# Выполните docker-compose build
+docker-compose build
+
+# Выполните docker-compose up -d
+docker-compose up -d
