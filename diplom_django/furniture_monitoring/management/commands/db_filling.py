b5041d37660d1697f376f422325c72b4bf5b69e2
Alexey Golyshev <kasperekd7@yandex.ru>
2024-06-03 23:30:20 +0300

    add place filling

--- diplom_django/furniture_monitoring/management/commands/db_filling.py
+++ diplom_django/furniture_monitoring/management/commands/db_filling.py
@@ -1,19 +1,16 @@
 import pandas as pd
 from django.core.management.base import BaseCommand, CommandError
 from ...models import Camera
+from ...models import Place
 
 class Command(BaseCommand):
     help = 'Imports cameras from a CSV file'
 
     def handle(self, *args, **kwargs):
-        # Укажите путь к CSV файлу прямо здесь
-        csv_file = 'csv/camera.csv'
-
-        try:
-            data = pd.read_csv(csv_file)
-        except Exception as e:
-            raise CommandError(f'Error reading CSV file: {e}')
-
+        
+        try:data = pd.read_csv('csv/camera.csv')
+        except Exception as e: ...
+        
         for _, row in data.iterrows():
             camera = Camera(
                 id=row['id'],
@@ -24,4 +21,16 @@ class Command(BaseCommand):
             camera.save()
 
             self.stdout.write(self.style.SUCCESS(f'Successfully added camera {camera.name}'))
+        
+        try:data = pd.read_csv('csv/place.csv')
+        except Exception as e: ...
+        
+        for _, row in data.iterrows():
+            place = Place(
+                id=row['id'],
+                name=row['name'],
+                description=row['description']
+            )
+            place.save()
 
+            self.stdout.write(self.style.SUCCESS(f'Successfully added camera {place.name}'))
